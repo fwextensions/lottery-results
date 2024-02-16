bf1665f382a98f54b10c2f01f9d1d1197a91ecd3
John Dunning <fw@johndunning.com>
2024-02-15 23:10:40 -0800

    Add the selected listing's name to the page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,31 @@
 import { useState } from "react";
 import { SingleValue } from "react-select";
+import Head from "next/head";
 import ListingPicker from "@/components/ListingPicker";
 import LotteryResults from "@/components/LotteryResults";
 
 export default function App()
 {
 	const [currentListing, setCurrentListing] = useState<Listing | undefined>();
+	const title = currentListing ? `Lottery Results - ${currentListing.Name}` : "";
 
 	const handleListingChange = (listing: SingleValue<Listing>) => {
 		setCurrentListing(listing ?? undefined);
 	}
 
 	return (
-		<main>
-			<header className="toolbar">
-				<ListingPicker onChange={handleListingChange} />
-			</header>
-			<LotteryResults listing={currentListing} />
-		</main>
+		<>
+			{title &&
+				<Head>
+					<title>{title}</title>
+				</Head>
+			}
+			<main>
+				<header className="toolbar">
+					<ListingPicker onChange={handleListingChange} />
+				</header>
+				<LotteryResults listing={currentListing} />
+			</main>
+		</>
 	);
 }
